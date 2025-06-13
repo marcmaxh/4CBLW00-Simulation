e6c6a83ffc028582203f9777b345880336598f3c
a1sh1 <alanxiuqishi@gmail.com>
2025-06-13 11:32:26 +0200

    Images for destinations

--- simulation/ui.py
+++ simulation/ui.py
@@ -4,6 +4,7 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from matplotlib.figure import Figure
+from PIL import Image, ImageTk
 import csv
 import os
 
@@ -46,45 +47,48 @@ class UI:
         self.draw_map(selected_var.get(), ax)
         canvas.draw()
 
-    def run_simulation(self):
-        self.update_values()
-        self.sim.set_time_of_day(self.tod)
-        result = self.sim.run_for_od_pair(self.start, self.end)
-        self.show_summary_plot(result)
-
 
     def show_summary_plot(self, results):
         summary = plotting.summarize_for_plot(results)
         fig = plotting.plot_summary(summary)
-
-        # Clear container
         self.clear_container()
 
-        # Embed plot
-        canvas = FigureCanvasTkAgg(fig, master=self.container)
-        canvas.draw()
-        canvas.get_tk_widget().pack(fill=tk.BOTH, expand=True)
+         # Create a frame to hold the plot
+        plot_frame = ttk.Frame(self.container)
+        plot_frame.pack(fill=tk.BOTH, expand=True)
 
-        # Back button
-        tk.Button(self.container, text="Back", command=self.show_tab3).pack(pady=10)
+        # Embed the matplotlib figure in the Tkinter UI
+        canvas = FigureCanvasTkAgg(fig, master=plot_frame)
+        canvas.draw()
+        canvas_widget = canvas.get_tk_widget()
+        canvas_widget.pack(fill=tk.BOTH, expand=True)
+
+        tk.Label(self.container, textvariable=self.status_var, fg='gray').pack(pady=5)
+
+        # Back to Tab 3
+        button_frame = ttk.Frame(self.container)
+        ttk.Button(button_frame, text="Back", command=self.show_tab3).pack(side="left", padx=10)
+        ttk.Button(button_frame, text="Restart", command=self.show_tab1).pack(side="right", padx=10)
 
     def update_values(self):
-        self.tod = self.time_var.get()
-        self.start = self.start_var.get()
-        self.end = self.end_var.get()
-        print("Time of Day: {self.tod}")
-        print(f"Start: {self.start}, End: {self.end}")
+        self.start = self.start_var.get() if hasattr(self, 'start_var') else 'N/A'
+        self.end = self.end_var.get() if hasattr(self, 'end_var') else 'N/A'
+        self.tod = self.time_var.get() if hasattr(self, 'time_var') else 'N/A'
+        self.weather = self.weather_var.get() if hasattr(self, 'weather_var') else 'N/A'
+        self.status_var.set(f"Origin: {self.start}, Destination: {self.end}, Time: {self.tod}, Weather: {self.weather}")
 
     def launch_ui(self):
         csv_path = os.path.join(self.base_dir, "simulation", "Origin to POI.csv")
         self.od_map = self.load_csv(csv_path)
         self.origin = sorted(self.od_map.keys())
+        self.origin.remove('\ufeffOrigin')
     
         # Create the main window
         self.root = tk.Tk()
         self.root.title("Simulation Controls")
         self.container = ttk.Frame(self.root)
         self.container.pack(padx=20, pady=20)
+        self.status_var = tk.StringVar()
         self.show_tab1()
         self.root.mainloop()
 
@@ -106,48 +110,104 @@ class UI:
         self.map_canvas = FigureCanvasTkAgg(fig, master=self.container)
         self.map_canvas.get_tk_widget().pack(pady=10)
 
+        tk.Label(self.container, textvariable=self.status_var, fg='gray').pack(pady=5)
 
         ttk.Button(self.container, text="Next", command=self.show_tab2).pack(pady=10)
 
     # Tab 2 - End point dropdown
     def show_tab2(self):
+        self.update_values()
         self.clear_container()
 
         selected_origin = self.start_var.get()
         valid_destinations = sorted(self.od_map.get(selected_origin, []))
+        self.dest_img_refs = []
+
 
         tk.Label(self.container, text="End Location").pack()
         self.end_var = tk.StringVar()
 
-        for zone in valid_destinations:
-            ttk.Radiobutton(self.container, text=zone, variable=self.end_var, value=zone).pack(anchor='w')
+        for dest in valid_destinations:
+            # ttk.Radiobutton(self.container, text=dest, variable=self.end_var, value=dest).pack(anchor='w')
+            frame = tk.Frame(self.container)
+            frame.pack(fill='x', pady=4, padx=10)
 
+            # Load image
+            img_path = os.path.join(self.base_dir, "image", f"{dest.replace('/', '(').replace('_', ' ')}.jpg")
+            if os.path.exists(img_path):
+                img = Image.open(img_path).resize((64, 64))
+                photo = ImageTk.PhotoImage(img)
+            else:
+                photo = None
+
+            # Destination name formatting
+            dest_name = dest.replace("_", " ").title()
+
+            # Radio button with image on the left
+            if photo:
+                img_label = tk.Label(frame, image=photo)
+                img_label.image = photo
+                img_label.pack(side='left', padx=(0, 10))
+                self.dest_img_refs.append(photo)
+
+            radio_btn = ttk.Radiobutton(frame, text=dest_name, variable=self.end_var, value=dest,
+                                        command=self.update_values)
+            radio_btn.pack(side='left')
+
+        # label
+        tk.Label(self.container, textvariable=self.status_var, fg='gray').pack(pady=5)
+        btn_frame = tk.Frame(self.container)
+        btn_frame.pack(pady=10, fill='x')
         ttk.Button(self.container, text="Back", command=self.show_tab1).pack(side="left", padx=10, pady=10)
         ttk.Button(self.container, text="Next", command=self.show_tab3).pack(side="right", padx=10, pady=10)
 
     # Tab 3
     def show_tab3(self):
+        self.update_values()
         self.clear_container()
-
         # Time
         tk.Label(self.container, text="Time of the day", font=("Arial", 14, "bold")).pack(pady=(10, 5))
-        self.time_var = tk.StringVar(value=self.time_of_day[3])
+        self.time_var = tk.StringVar()
 
         for time in self.time_of_day:
             display_time = time.replace("_", " ").title()
-            ttk.Radiobutton(self.container, text=display_time, variable=self.time_var, value=time).pack(anchor='w')
+            ttk.Radiobutton(self.container, text=display_time, variable=self.time_var, value=time, 
+                            command=self.update_values).pack(anchor='w')
 
         # Weather
         tk.Label(self.container, text="Weather", font=("Arial", 14, "bold")).pack(pady=(15, 5))
-        self.weather_var = tk.StringVar(value=self.weather_types[0])
+        self.weather_var = tk.StringVar()
 
         for weather in self.weather_types:
-            ttk.Radiobutton(self.container, text=weather.title(), variable=self.weather_var, value=weather).pack(anchor='w')
+            ttk.Radiobutton(self.container, text=weather.title(), variable=self.weather_var, value=weather, 
+                            command=self.update_values).pack(anchor='w')
 
 
+        tk.Label(self.container, textvariable=self.status_var, fg='gray').pack(pady=5)
+        
         # Submit button
         ttk.Button(self.container, text="Back", command=self.show_tab2).pack(side="left", padx=10, pady=10)
-        tk.Button(self.container, text="Start Simulation", command=self.run_simulation).pack(side = "right", padx=10, pady=10)
+        tk.Button(self.container, text="Start Simulation", command=self.show_tab4).pack(side = "right", padx=10, pady=10)
+
+    # Tab 4
+    def show_tab4(self):
+        self.update_values()
+        self.clear_container()
+
+        # Optional: Indicate simulation is running
+        loading_label = tk.Label(self.container, text="Running simulation...", font=("Arial", 12, "italic"))
+        loading_label.pack(pady=20)
+        self.root.update_idletasks()  # Force immediate update of the UI
+
+        # Run the simulation
+        self.sim.set_time_of_day(self.tod)
+        result = self.sim.run_for_od_pair(self.start, self.end)
+
+        # Remove loading label
+        loading_label.destroy()
+
+        # Show the plot
+        self.show_summary_plot(result)
 
     def clear_container(self):
         for widget in self.container.winfo_children():
